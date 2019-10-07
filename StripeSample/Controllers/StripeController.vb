2c2b59afd4399826a7859a01ea041fc14aea7e88
helix46 <bryce.abbott@iblong2.com>
2019-10-08 08:19:48 +1000

    Init commit

---
+++ StripeSample/Controllers/StripeController.vb
@@ -0,0 +1,53 @@
+﻿'Imports System.Web.Mvc
+
+'Namespace Controllers
+'    Public Class StripeController
+'        Inherits Controller
+
+'        ' GET: Stripe
+'        Function Index() As ActionResult
+'            Return View()
+'        End Function
+'    End Class
+'End Namespace
+
+Imports System.Data.SqlClient
+Imports System.Collections.Generic
+Imports System.Linq
+Imports System.Net
+Imports System.Net.Http
+Imports System.Web.Http
+Imports System.Security.Principal
+
+Public Structure StructStripe
+    Public chargeToken As String
+    Public amount As Decimal
+End Structure
+
+Namespace Controllers
+    <RoutePrefix("api/stripe")>
+    Public Class StripeController
+        Inherits ApiController
+
+        <HttpPost> <Route> '<Authorize>
+        Public Async Function [post](StructStripe As StructStripe) As Threading.Tasks.Task(Of IHttpActionResult)
+            Try
+                Dim ProductionMode = False
+                Dim StripeApiSecretKeyLive = ""
+                Dim StripeApiSecretKeyDev = "sk_test_xxxxxxxxxxxxxxxxxxxxxxxx"
+                Dim StructStripePaymentReturn As StructStripePaymentReturn = Await StripeService.makeStripeCharge(StructStripe.amount, StructStripe.chargeToken, ProductionMode, StripeApiSecretKeyLive, StripeApiSecretKeyDev)
+                If StructStripePaymentReturn.Paid Then
+                    'save to database, send receipt etc
+                    Return Ok("Charge successful")
+                Else
+                    Return Ok(StructStripePaymentReturn.ErrorMessage)
+                End If
+
+            Catch ex As Exception
+                ' handle error
+                Return InternalServerError(ex)
+            End Try
+        End Function
+
+    End Class
+End Namespace
---
+++ StripeSample/Services/StripeService.vb
@@ -0,0 +1,80 @@
+﻿Imports System.Threading.Tasks
+Imports Stripe
+
+Public Structure StructStripePaymentReturn
+    Public Paid As Boolean
+    Public ErrorMessage As String
+    Public MerchantFee As Decimal
+    Public GST As Decimal
+    Public charge As Charge
+End Structure
+
+Public Class StripeService
+    Public Shared Async Function makeStripeCharge(ByVal AmountToPay As Decimal, ByVal stripeToken As String, ByVal ProductionMode As Boolean, ByVal StripeApiSecretKeyLive As String, ByVal StripeApiSecretKeyDev As String) As Task(Of StructStripePaymentReturn)
+        If ProductionMode Then
+            StripeConfiguration.ApiKey = StripeApiSecretKeyLive
+        Else
+            StripeConfiguration.ApiKey = StripeApiSecretKeyDev
+        End If
+
+        Dim chargeService = New ChargeService()
+
+        Dim options As ChargeCreateOptions = New ChargeCreateOptions() With {
+            .Amount = System.Convert.ToInt32((AmountToPay * CDec(100))),
+            .Currency = "aud",
+            .Description = "Description to show on receipt sent by Stripe",
+            .Source = stripeToken,
+            .StatementDescriptor = "Retail Edge" 'Description to show on CC statement (22 chars?)
+        }
+
+        Dim charge As Charge = New Charge()
+
+        Try
+            charge = Await chargeService.CreateAsync(options)
+        Catch ex As Exception
+            Dim structStripePaymentReturn As StructStripePaymentReturn
+            structStripePaymentReturn.Paid = False
+            structStripePaymentReturn.ErrorMessage = ex.Message
+            structStripePaymentReturn.charge = charge
+            structStripePaymentReturn.MerchantFee = 0
+            structStripePaymentReturn.GST = 0
+            Return structStripePaymentReturn
+        End Try
+
+
+        If charge.Paid Then
+            Dim TotalCharged = charge.Amount / 100
+            Dim MerchantFee As Decimal = CDec(0)
+            Dim GST As Decimal = CDec(0)
+
+            Dim balanceTransactionService As New BalanceTransactionService
+            Dim balanceTransaction As BalanceTransaction = balanceTransactionService.Get(charge.BalanceTransactionId)
+
+            For Each feeDetail In balanceTransaction.FeeDetails
+                If feeDetail.Type = "stripe_fee" Then
+                    MerchantFee += System.Convert.ToDecimal(feeDetail.Amount) / 100
+                End If
+
+                If feeDetail.Type = "tax" Then
+                    GST += System.Convert.ToDecimal(feeDetail.Amount) / 100
+                End If
+            Next
+
+            Dim structStripePaymentReturn As StructStripePaymentReturn
+            structStripePaymentReturn.MerchantFee = MerchantFee
+            structStripePaymentReturn.GST = GST
+            structStripePaymentReturn.Paid = True
+            structStripePaymentReturn.ErrorMessage = ""
+            structStripePaymentReturn.charge = charge
+            Return structStripePaymentReturn
+        Else
+            Dim structStripePaymentReturn As StructStripePaymentReturn
+            structStripePaymentReturn.MerchantFee = 0
+            structStripePaymentReturn.GST = 0
+            structStripePaymentReturn.charge = charge
+            structStripePaymentReturn.Paid = False
+            structStripePaymentReturn.ErrorMessage = charge.FailureMessage
+            Return structStripePaymentReturn
+        End If
+    End Function
+End Class
